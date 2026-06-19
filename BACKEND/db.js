d139b6d7169209e2c33529abe857ee2a881a1767
Zeeshan Siddiqui <mzeeshancs8990@gmail.com>
2026-06-19 22:24:55 +0500

    Database Password Reset

--- BACKEND/db.js
+++ BACKEND/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'SQLmzs2006',          // your MySQL password
+    password: '---',          // your MySQL password
     database: 'ssuet_qr_library',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 module.exports = pool;
+
